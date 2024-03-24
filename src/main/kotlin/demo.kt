38ca59a6b9811114322b917414c706e52c76fdd9
Bram Janssens <s.a.janssens@gmail.com>
2024-03-24 13:32:08 +0100

    Typos and so.

--- src/main/kotlin/demo.kt
+++ src/main/kotlin/demo.kt
@@ -1,7 +1,7 @@
 fun main() {
     println("Seb is een ei 🥚")
-    val b1 = Breuk(100, 101)
-    val b2 = Breuk(33, 34)
+    val b1 = Breuk(1, 4)
+    val b2 = Breuk(1, 3)
     val som = b1 + b2
     println(som)
 }
@@ -9,8 +9,8 @@ fun main() {
 data class Breuk(val teller: Int, val noemer: Int) {
     operator fun plus(other: Breuk): Breuk {
         val nieuwenoemer = this.noemer * other.noemer
-        val nieuweteller1 =this.teller * other.noemer
-        val nieuweteller2 =other.teller * this.noemer
+        val nieuweteller1 = this.teller * other.noemer
+        val nieuweteller2 = other.teller * this.noemer
         val nieuweteller = nieuweteller1 + nieuweteller2
         val nieuwebreuk = Breuk(nieuweteller, nieuwenoemer)
         return nieuwebreuk
